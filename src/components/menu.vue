c5eeec5713a5ddffcda369b62bae190afc1fb495
JiangShuuu <globelex65@gmail.com>
2022-08-26 17:52:38 +0800

    upd id

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,6 +3,7 @@
     <h1 class="mr-4 text-3xl">Menu</h1>
     <router-link to="/">回首頁</router-link>
     <router-link to="/items">items</router-link>
+    <router-link to="/test-id-page/123">testIdPage</router-link>
     <router-link to="/123213">404</router-link>
   </menu>
 </template>
---
+++ src/pages/test-id-page/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+</script>
+
+<template>
+  <p>test-id-page/{{ route.params.id }}</p>
+</template>
