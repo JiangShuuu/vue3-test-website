5370fdaef217a6b3fe8af1f60de2d0d45e3b6f8e
JiangShuuu <globelex65@gmail.com>
2022-09-07 10:35:59 +0800

    upd config

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,11 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 
 const router = createRouter({
-  history: createWebHistory('vue3-test-website'),
+  history: createWebHistory(),
   routes: setupLayouts(generatedRoutes)
 })
 
 router.beforeEach(async (to, from, next) => {
-  console.log('toPathss', to.meta)
   next()
 })
 
